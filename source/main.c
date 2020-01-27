1c7995613c2d49faed38973d7016b37c16515eb4
dukeph0 <dpham5161@outlook.com>
2020-01-26 21:49:40 -0800

    Add main.c

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: dukeph0 - Duke Pham
  *  Partner(s) Name: Juvenal Ortega - Email: jorte057@ucr.edu
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 022
+ *	Assignment: Lab 5  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,14 +13,12 @@
 #endif
 
 /*
- * (From an earlier lab) A car has a fuel-level sensor that sets PA3..PA0
- * to a value between 0 (empty) and 15 (full).
- * A series of LEDs connected to PC5..PC0 should light to graphically indicate the fuel level.
- * If the fuel level is 1 or 2, PC5 lights. 
- * If the level is 3 or 4, PC5 and PC4 light. 5-6 lights PC5..PC3.
- * 7-9 lights PC5..PC2. 10-12 lights PC5..C1. 13-15 lights PC5..PC0.
- * Also, PC6 connects to a "Low fuel" icon, which should light if the level is 4 or less.
- * Use buttons on PA3..PA0 and mimic the fuel-level sensor with presses. 
+ * (From an earlier lab) Buttons are connected to PA0 and PA1. 
+ * Output for PORTC is initially 0.
+ * Pressing PA1 decrements PORTC (stopping at 0).
+ * If both buttons are depressed (even if not initially simultaneously), PORTC resets to 0.
+ * If a reset occurs, both buttons should be fully released before additional increments or decrements are allowed to happen.
+ * Use LEDs (and resistors) on PORTC. Use a state machine (not synchronous) captured in C.  
  */
 
 int main(void) {
@@ -28,63 +26,100 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // configure port A's pins as inputs initialize with 1s
 	DDRC = 0xFF; PORTC = 0x00; // configure port C's pins as outputs initialize to 0s
 
-	unsigned char tmpA = 0x00;
+	addsm = init;
 
     /* Insert your solution below */
     while (1) {
-	    tmpA = ~PINA;
-
-	    switch (tmpA){
-
-		    case (0x00) : PORTC = 0x40; // no light, low fuel warning (PC^)
-		    break;
-
-			case (0x01) : PORTC = 0x60; // PC5 lights on, low fuel warning (PC6)
-			break;
-
-			case (0x02) : PORTC = 0x60; //PC5 lights, low fuel warning (PC6)
-            break;
-		
-			case (0x03) : PORTC = 0x70; //PC5, PC4 lights on, low fuel warning (PC6)
-            break;
-		
-			case (0x04) : PORTC = 0x70; //PC5, PC4 lights on, low fuel warning (PC6)
-            break;
-			
-			case (0x05) : PORTC = 0x38; //PC5, PC4, PC3  lights on, no fuel warning (PC6)
-            break;
-			
-			case (0x06) : PORTC = 0x38; //PC5, PC4, PC3  lights on, no fuel warning (PC6)
-            break;
-		
-			case (0x07) : PORTC = 0x3C; //PC5, PC4, PC3, PC2  lights on, no fuel warning (PC6)
-            break;
-		
-			case (0x08) : PORTC = 0x3C; //PC5, PC4, PC3, PC2  lights on, no fuel warning (PC6)
-            break;
-
-			case (0x09) : PORTC = 0x3C; //PC5, PC4, PC3, PC2  lights on, no fuel warning (PC6)
-            break;
-
-			case (0x0A) : PORTC = 0x3E; //PC5, PC4, PC3, PC2, PC1 lights on, no fuel warning
-            break;
-			
-			case (0x0B) : PORTC = 0x3E; //PC5, PC4, PC3, PC2, PC1  lights on, no fuel warning
-            break;
-			
-			case (0x0C) : PORTC = 0x3E; //PC5, PC4, PC3, PC2, PC1  lights on, no fuel warning
-            break;
-
-			//13+ fuel
-			default : PORTC = 0x3F; // PC5, PC4, PC3, PC2, PC1 light no warning
-			break;
-
-
-
-	    
-	    
-	    
+		tick();
 	    }
     }
-    return 1;
+}
+
+void tick(){
+	switch(addsm) {
+		case init:
+			PORTC = 0x07;
+			addsm = interphase;
+			break;
+		case interphase:
+			//PA0 & !PA1 go to inc
+			if ((~PINA & 0x03) == 0x01){
+				addsm = inc;
+			}
+			//PA1 & !PA0 go to dec
+			else if ((~PINA & 0x03) == 0x02){
+				addsm = dec;
+			}
+			//!PA0 and !PA1 go to reset
+			else if ((~PINA & 0x03) == 0x03){
+				addsm = reset;
+			} else {// stay in interphase for anyrthing else
+				addsm = interphase;
+			}
+			break;
+		case inc:
+			addsm = incWait;
+			break;
+		case incWait:
+			//wait for button release 
+			if ((~PINA & 0x03) == 0x01){
+				addsm = incWait;
+			}else if ((~PINA & 0x03) == 0x03){
+				addsm = reset;
+			}else{
+				addsm = interphase;
+			}
+			break;
+		case dec:
+			addsm = decWait;
+			break;
+		case decWait:
+			// wait for button release
+                        if ((~PINA & 0x03) == 0x02){
+                                addsm = decWait;
+			} else if ((~PINA & 0x03) == 0x03){
+                                addsm = reset;
+                        }else{
+                                addsm = interphase;
+                        }
+
+			break;
+		case reset:
+			if ((~PINA & 0x03) == 0x00){
+				addsm = interphase;
+			} else {
+				addsm = reset;
+			}
+			break;
+		default:
+			addsm = interphase;
+			break;
+	}//transitions
+	
+	//state actions
+	switch(addsm) {
+                case init:
+                        break;
+                case interphase:
+			break;
+                case inc:
+			// Add 1 to PORTC but doesn't surpass 9
+			if (PORTC < 0x09)
+				PORTC = (PORTC + 0x01);
+                        break;
+		case incWait:
+			break;
+                case dec:
+			//Subtract 1 to PORTC but doesn't go below 0
+			if (PORTC > 0x00)
+                        	PORTC = (PORTC - 0x01);
+                        break;
+		case decWait:
+			break;
+                case reset:
+			PORTC = 0x00;
+                        break;
+                default:
+                        break;
+	}
 }
